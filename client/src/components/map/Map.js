2f8922a50694aad0d939e908f1cc2d82245cca1b
Arturo Larios <pinsasmetal@gmail.com>
2020-09-21 00:33:08 -0500

    map added

--- client/src/components/map/Map.js
+++ client/src/components/map/Map.js
@@ -1,10 +1,31 @@
 import React from 'react';
+import GoogleMapReact from 'google-map-react';
+import './map.css';
+
+const Marker = ({text}) => <div>{text}</div>;
 
 class Map extends React.Component {
+    static defaultProps = {
+        center: {
+            lat: 59.95,
+            lng: 30.33
+        },
+        zoom: 11
+    };
+
     render() {
         return(
-            <div>
-                
+            <div id="map">
+                <GoogleMapReact
+                    bootstrapURLKeys = {{key: "AIzaSyBDaeWicvigtP9xPv919E-RNoxfvC-Hqik"}}
+                    defaultCenter = {this.props.center}
+                    defaultZoom = {this.props.zoom}
+                >
+                    <Marker
+                        lat={59.955413}
+                        lng={30.337844}
+                        text="My Marker"/>
+                </GoogleMapReact>
             </div>
         );
     }
